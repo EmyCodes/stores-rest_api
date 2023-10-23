f9a23262f5952eba84c3959fad5ae915fce8c30d
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-10-23 09:36:06 +0100

    chore: updated

--- flask-mysql/app.py
+++ flask-mysql/app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://emycodes:argv[1]@localhost'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://"root":19022002@localhost/5050'
 db = SQLAlchemy(app)
 
 if __name__ == "__main__":
