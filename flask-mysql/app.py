da785b2cdfd192044108edcae937808264e064f0
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-11 20:17:11 +0100

    feat: test running

--- flask-mysql/app.py
+++ flask-mysql/app.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python3.11
+#!/usr/bin/python3
 
-from sys import argv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from db import username, host, password
 
 app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:19022002@localhost:3306/Emy'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{host}/flask_mysql'
 db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
