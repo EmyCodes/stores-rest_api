adba6b4e5857c95548771198ab2df93a2aba88ad
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-11 23:21:38 +0100

    feat: update file

--- flask-mysql/app.py
+++ flask-mysql/app.py
@@ -5,9 +5,29 @@ from flask_sqlalchemy import SQLAlchemy
 from db import username, host, password
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{host}/flask_mysql'
+db_uri = f'mysql+pymysql://{username}:{password}@{host}/flask_mysql'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy(app)
+
+
+class Author(db.Model):
+    """
+    Starting our to create an author database application
+    which will provide RESTful CRUD APIs
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    specialisation = db.Column(db.String(50))
+
+    def __init__(self, name, specialisation):
+        self.name = name
+        self.specialisation = specialisation
+
+    def __repr__(self):
+        return f"Product {self.id}"
+
+
+db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
