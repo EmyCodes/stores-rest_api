1d0c16c236f303fb841a798d09a0bd4e36954b8f
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-11 23:34:40 +0100

    fix: fixes RunTime Error

--- flask-mysql/app.py
+++ flask-mysql/app.py
@@ -27,7 +27,7 @@ class Author(db.Model):
         return f"Product {self.id}"
 
 
-db.create_all()
-
 if __name__ == "__main__":
-    app.run(debug=True)
+	with app.app_context():
+		db.create_all()
+	app.run(debug=True)
