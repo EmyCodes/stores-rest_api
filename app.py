2eb9e3bb20f1ac40ae9cee135aa1ed8f2966b8f2
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-20 00:44:56 +0100

    feat: add new file

---
+++ app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+			}
+		]
+	}
+]
+
+@app.get("/store")
+def get_stores():
+    return [{"stores": stores}]
