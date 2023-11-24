b18a3ca349ffef3b7f6233a084c5d05368ec944e
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-24 15:24:41 +0100

    build: update existing endpoint, handle exception

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, request
+from flask import Flask, request, abort
 from uuid import uuid4
 from db import stores, items
 
@@ -18,9 +18,28 @@ def get_all_stores():
 
 
 @app.post("/store")
-def post_stores():
+def create_store():
     """This endpoint POST new store with a unique id"""
     store_data = request.get_json()
+    #Error handling
+    if (
+        "name" not in store_data
+    ):
+        abort(400, message="Bad Request! Ensure 'name', 'price',\
+              'store_id' are in the JSON Payload")
+        
+    # Check if the key exists
+    for store in stores.values():
+        if (
+            store_data["name"] == store ["name"] and
+            store_data["store_id"] == store[store_id]
+        ):
+            abort(400, message="Store Already Exist")
+
+    if store_data["store_id"] not in stores:
+    # if store_data["store_id"] != stores[store_id]:    
+        abort(400, message="Store Not Found")
+    # Else: Update database with the Processed Logic
     store_id = uuid4().hex
     new_store = {**store_data, "id": store_id}
     # new_store = {**request_store, id}
@@ -32,6 +51,29 @@ def post_stores():
 def create_item():
     """This endpoint CREATE new store"""
     item_data = request.get_json()
+
+    #Error handling
+    if (
+        "name" not in item_data or
+        "price" not in item_data or
+        "item_id" not in item_data
+    ):
+        abort(400, message="Bad Request! Ensure 'name', 'price',\
+              'store_id' are in the JSON Payload")
+        
+    # Check if the key exists
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"] and
+            item_data["item_id"] == item[item_id]
+        ):
+
+            abort(400, message="Store Already Exist")
+    
+    if item_data["item_id"] not in items:
+    # if store_data["store_id"] != stores[store_id]:    
+        abort(400, message="Item Not Found")
+    # Else: Update database with the Processed Logic
     # if item_data[store_id] not in stores:
     #     return f"Message: {name} not found", 404
     item_id = uuid4().hex
@@ -52,7 +94,7 @@ def get_one_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"Message": "Store Not Found"}, 404
+        abort (400, Message="Store Not Found")
 
 
 @app.get("/item/<string:item_id>")
@@ -62,4 +104,4 @@ def get_one_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"Message": "Item Not Found"}, 404
+        abort (400, Message="Item Not Found")
