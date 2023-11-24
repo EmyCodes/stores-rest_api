528795b8fb7693280d205c670aed89b8173cf833
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-24 13:13:23 +0100

    doc: add documentation and pass PEP8 style

--- app.py
+++ app.py
@@ -4,45 +4,60 @@ from flask import Flask, request
 from uuid import uuid4
 from db import stores, items
 
+"""
+This is my first API building Project
+using Flask
+"""
 app = Flask(__name__)
 
 
 @app.get("/store")
 def get_all_stores():
+    """This endpoint GET ALL stores"""
     return [{"stores": list(stores.values())}]
+
 
 @app.post("/store")
 def post_stores():
+    """This endpoint POST new store with a unique id"""
     store_data = request.get_json()
-    store_id = uuid4().hex        #feeko93eheue493dhde3
+    store_id = uuid4().hex
     new_store = {**store_data, "id": store_id}
     # new_store = {**request_store, id}
     stores[store_id] = new_store
     return new_store, 201
 
+
 @app.post("/items")
 def create_item(name):
+    """This endpoint CREATE new store"""
     item_data = request.get_json()
     if item_data["store_id"] not in stores:
         return f"Message: {name} not found", 404
-    item_id = uuid4().hex        #feeko93eheue493dhde3
+    item_id = uuid4().hex
     new_item = {**item_data, "id": item_id}
     stores[item_id] = new_item
     return new_item, 201
 
+
 @app.get("/items")
 def get_all_items():
-    return list(items.values())    
+    """This endpoint GET ALL new items"""
+    return list(items.values())
+
 
 @app.get("/store/<string:store_id>")
 def get_one_store(store_id):
+    """This endpoint GET a specific store by store_id"""
     try:
         return stores["store_id"]
     except KeyError:
         return "Message: Store Not Found", 404
 
+
 @app.get("/items/<string:item_id")
 def get_one_item(item_id):
+    """This endpoint GET a specific item by item_id"""
     # if items["item_id"] not in items:
     try:
         return items["item_id"]
