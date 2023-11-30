035d3de6e2aad9e64029f06e7aee2d6a209b88c7
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-30 20:48:09 +0100

    fix: fix Operational Error

--- models/item.py
+++ models/item.py
@@ -9,4 +9,4 @@ class ItemModel(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
     store_id = db.Column(db.Integer, db.ForeignKey("Stores.id"), unique=False, nullable=False)
-    store = db.relationship("StoreModel", back_populate="items")
+    store = db.relationship("StoreModel", back_populates="items")
