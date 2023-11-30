3727c8b8dbd41f69d93da9f23f018e3e61c42d1e
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-30 19:06:25 +0100

    feat: add Stores Model

---
+++ models/store.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from db import db
+
+class StoreModel(db.Model):
+    """Docs: ---To be Updated Later"""
+    __tablename__ = "Stores"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
