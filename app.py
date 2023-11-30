e83137bd6ce8227503c62a681720daaec0283ca6
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-01 00:25:28 +0100

    chore: rename placeholder

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def create_app(db_url=None):
     app.config["OPENAPI_URL_PREFIX"] ="/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or  getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or  getenv("DATABASE_URL", f"sqlite:///{database}")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)                                     
 
