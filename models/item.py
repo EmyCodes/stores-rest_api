bc70f018ad05c36511251095c150bf2a56c4b0a6
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-11-30 19:00:34 +0100

    feat: add item Model

---
+++ models/item.py
@@ -0,0 +1,9 @@
+from db import db
+
+class ItemModel(db.Model):
+    """Doc here--- To be updated later"""
+    __tablename__ = "items"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    price = db.Column(db.Float(precision=2), unique=False, nullable=False)
+    store_id = db.Column(db.Integer, unique=False, nullable=False)
