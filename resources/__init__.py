50ebcca123b434e2c48c0fc49398c760b69ee966
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-06 00:50:11 +0100

    chore: add and comment out __init__.py

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
-from resources.item import blp as ItemBlueprint
-from resources.store import blp as StoreBlueprint
+# from resources.item import blp as ItemBlueprint
+# from resources.item import Items, ItemList
+# from resources.store import blp as StoreBlueprint
+# from resources.store import Store, StoreList
