57e686be70d8ab078ab71a000426b006c67e2b4f
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-07 21:11:30 +0100

    feat: add update/put endpoint

--- resources/item.py
+++ resources/item.py
@@ -29,8 +29,17 @@ class Store(MethodView):
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        item = ItemModel.query.get_or_404(item_id)
-        raise NotImplementedError("Updating an item is not implemented")
+        item = ItemModel.query.get(item_id)
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id, **item)
+
+        db.session.add(item)
+        db.session.commit()
+
+        return item
 
 
 @blp.route("/item")
