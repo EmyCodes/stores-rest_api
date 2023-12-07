e14b404d706a8c9b01fe80701369de4f24103f2f
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-08 00:31:26 +0100

    chore: slightly modify

--- models/item.py
+++ models/item.py
@@ -4,9 +4,9 @@ from db import db
 
 class ItemModel(db.Model):
     """Doc here--- To be updated later"""
-    __tablename__ = "Items"
+    __tablename__ = "items"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
-    store_id = db.Column(db.Integer, db.ForeignKey("Stores.id"), unique=False, nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False)
     store = db.relationship("StoreModel", back_populates="items")
--- models/store.py
+++ models/store.py
@@ -4,7 +4,7 @@ from db import db
 
 class StoreModel(db.Model):
     """Docs: ---To be Updated Later"""
-    __tablename__ = "Stores"
+    __tablename__ = "stores"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     items = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
