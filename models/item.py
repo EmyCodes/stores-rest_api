001c4dee0d66c58d2bab419512a59eb4a54b9c06
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-08 08:36:38 +0100

    feat: create relationship between items and tags

--- models/item.py
+++ models/item.py
@@ -9,4 +9,6 @@ class ItemModel(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False)
+    
     store = db.relationship("StoreModel", back_populates="items")
+    tags = db.relationship("TagModel", back_populates="items", secondary="items_tags")
