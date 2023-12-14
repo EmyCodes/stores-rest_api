341c4e26c76b466e644e85655b116096e626c28f
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-14 09:45:20 +0100

    feat: update with user schema

--- schemas.py
+++ schemas.py
@@ -52,3 +52,9 @@ class TagAndItemSchema(Schema):
     item = fields.Nested(ItemSchema)
     tag = fields.Nested(TagSchema)
     
+class UserSchema(Schema):
+    """Docs: To be updated """
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
+     
