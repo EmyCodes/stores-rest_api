c9c8cc36595df6d8383fe30ed4ded96f95d1d1f8
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-15 18:11:05 +0100

    feat: handle error

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
-from flask import Flask
+import jwt
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from os import getenv
@@ -41,6 +41,40 @@ def create_app(db_url=None):
 
     app.config["JWT_SECRET_KEY"] = "106687186741913238732192922019664271153"
     jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expire_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "message":"The token has expired.",
+                    "error":"token_expired"
+                }),
+                401,
+        )
+    
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed",
+                 "error": "invali_token"
+                }
+            ),
+            401,
+        )
+    
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
 
     # Create all Tables in Database
     with app.app_context():
