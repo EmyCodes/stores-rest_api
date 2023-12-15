a3887240455fa006f28db3c1ee9a6734ef1e1bd7
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-15 18:59:46 +0100

    chore: correct typo error

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def create_app(db_url=None):
 
     @jwt.token_in_blocklist_loader
     def check_if_token_in_blocklist(jwt_header, jwt_payload):
-        return jwt_payload["jt1"] in BLOCKLIST
+        return jwt_payload["jti"] in BLOCKLIST
 
     @jwt.revoked_token_loader
     def revoked_token_callback(jwt_header, jwt_payload):
