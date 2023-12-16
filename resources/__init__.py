65fd08d4a72a176a23f60394ff847de9d2480c82
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-16 11:52:07 +0100

    chore: empty the file

--- resources/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-# from resources.item import blp as ItemBlueprint
-# from resources.item import Items, ItemList
-# from resources.store import blp as StoreBlueprint
-# from resources.store import Store, StoreList
