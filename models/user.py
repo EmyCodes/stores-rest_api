01a1d2598671391c9d69f87f6f65c968c7e5ca8d
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-12-21 09:23:35 +0100

    docs: update with documentation

--- models/user.py
+++ models/user.py
@@ -4,7 +4,14 @@ from db import db
 
 
 class UserModel(db.Model):
-    """Docs: To be updated """
+    """
+    User Model for creating a login object
+    Args:
+        __tablename__ (str): The name of the table in the database
+        id (int): The id of the user
+        username (str): The username of the user
+        password (str): The password of the user
+    """
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
